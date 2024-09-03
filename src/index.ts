d9446468a17d2324fd2c6c6908d025a630532086
hendrata.loekman@gmail.com <hendrata.loekman@gmail.com>
2024-09-03 18:25:49 +0700

    Add user authentication endpoints

--- src/index.ts
+++ src/index.ts
@@ -23,3 +23,16 @@ app.listen(port, () => {
     console.log(`Server running on port ${port}`);
 });
 
+// Endpoint untuk register user
+app.post('/register', (req: Request, res: Response) => {
+    const { username, password } = req.body;
+    // Logika untuk register user
+    res.send('User registered');
+});
+
+// Endpoint untuk login user
+app.post('/login', (req: Request, res: Response) => {
+    const { username, password } = req.body;
+    // Logika untuk login user
+    res.send('User logged in');
+});
